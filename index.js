55ac30fe0b38115666b1624dbc37e8c07428df54
MiMNets <mdmonirulislammonir@gmail.com>
2020-10-20 18:01:48 +0600

    port updated

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Server is listening at http://localhost:${port}`)
 })
